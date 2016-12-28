9eef47407b5700fd74d689e32e979837a52b5427
Adam Scott <adam@technocreatives.com>
2016-12-28 20:49:06 +0100

    edit to question flow

--- client/main.js
+++ client/main.js
@@ -47,7 +47,39 @@ Template.QuestionCard.events({
         clone.css({"position": "absolute", "margin":"auto", "background": "white"});
         clone.animate({'width' : '70%', 'height' : '70%', 'opacity' : 1, 'top' : "15%", 'left' : "15%"}, 1000, function(){
             clone.children('h2').first().css({"visibility":"visible", 'opacity':0})
-            clone.children('h2').first().animate({'opacity':1}, 1000)
+            clone.children('p').first().animate({'font-size':'22pt'}, 700, function(){
+                //clone.children('h2').first().animate({'opacity':1}, 2000)
+                //fadein letter by letter
+                $(function() {
+
+                  //get the welcome msg element
+                  var $all_msg = clone.children('h2').first()
+                  //get a list of letters from the welcome text
+                  var $wordList = clone.children('h2').first().text().split("");
+                  //clear the welcome text msg
+                  clone.children('h2').first().text("");
+                  clone.children('h2').first().css({'opacity':1})
+
+                  //loop through the letters in the $wordList array
+                  $.each($wordList, function(idx, elem) {
+                      console.log('loop')
+                    //create a span for the letter and set opacity to 0
+                    var newEL = $("<span/>").text(elem).css({
+                      opacity: 0
+                    });
+                    //append it to the welcome message
+                    newEL.appendTo($all_msg);
+                    //set the delay on the animation for this element
+                    newEL.delay(idx * 70);
+                    //animate the opacity back to full 1
+                    newEL.animate({
+                      opacity: 1
+                  }, 1000);
+                  });
+
+                });
+            })
+
         })
         instance.$(questionCard).animate({'width' : '0%'}, 400, function() {
             instance.$(questionCard).css({"visibility": "hidden"})
